b7a11f1980be747477f7548f63544baf63cb2a7f
John Ghidiu <jghidiu@solutechnology.com>
2018-05-28 19:08:17 -0400

    Removed console faking

--- src/SettingsManager.js
+++ src/SettingsManager.js
@@ -5,41 +5,13 @@
 (function(root, factory) {
     'use strict';
 
-    // Try to define a console object
-    (function(){
-        try {
-            if (!console && ('undefined' !== typeof window)) {
-                // Define the console if it does not exist
-                if (!window.console) {
-                    window.console = {};
-                }
-
-                // Union of Chrome, FF, IE, and Safari console methods
-                var consoleFunctions = [
-                    'log', 'info', 'warn', 'error', 'debug', 'trace', 'dir', 'group',
-                    'groupCollapsed', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd',
-                    'dirxml', 'assert', 'count', 'markTimeline', 'timeStamp', 'clear'
-                ];
-                // Define undefined methods as no-ops to prevent errors
-                for (var i = 0; i < consoleFunctions.length; i++) {
-                    if (!window.console[consoleFunctions[i]]) {
-                        window.console[consoleFunctions[i]] = function() {};
-                    }
-                }
-            }
-        } catch(error) {
-            // Not much to do if there is no console
-        }
-
-    })();
-
     // Determine the module system (if any)
-    if (typeof define === 'function' && define.amd) {
+    if ('function' === typeof define && define.amd) {
         // AMD
         define(factory);
     } else {
         // Node
-        if (typeof exports !== 'undefined') {
+        if (undefined !== typeof exports) {
             module.exports = factory();
         } else {
             // None

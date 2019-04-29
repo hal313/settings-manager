4031100946511e5208d29567e9b7cc00445ce86e
John Ghidiu <john@rmdashrf.org>
2019-04-29 16:46:38 -0400

    Added TODOs to releae script

--- ext/scripts/release.sh
+++ ext/scripts/release.sh
@@ -60,6 +60,7 @@ git add CHANGELOG.md
 git commit -m 'Updated changelog'
 
 ## Merge into master
+## TODO: Figure out how to accept default message
 git checkout master
 git pull origin master
 git merge --no-ff release/${NEXT_VERSION}
@@ -69,6 +70,7 @@ git tag -a -m 'Tagged for release' ${NEXT_VERSION}
 git branch -d release/${NEXT_VERSION}
 
 ## Merge down to develop
+## TODO: Figure out how to accept default message
 git checkout develop
 git merge --no-ff master
 
